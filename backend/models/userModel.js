3c05e7577599fe78b285631462eb859087bdbaa1
litioner <litioner@protonmail.com>
2025-03-07 09:59:58 +0200

    Add user and job models with schema definitions

---
+++ backend/models/userModel.js
@@ -0,0 +1,12 @@
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone_number: { type: String, required: true },
+    gender: { type: String, required: true },
+    date_of_birth: { type: Date, required: true },
+    membership_status: { type: String, required: true },
+    bio: { type: String },
+    address: { type: String, required: true },
+    profile_picture: { type: String, required: false }
+}, { timestamps: true, versionKey: false });
